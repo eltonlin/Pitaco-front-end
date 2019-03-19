614ed7aff555dd8f80fe76ca36970744e6fd62dd
Renato Jales <34494310+renatojales@users.noreply.github.com>
2019-03-18 22:54:31 -0300

    Add files via upload
    Criação de telas finais

---
+++ Pitaco/Pitaco/src/components/admin/AdminPages.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="admin-pages">
+        <PageTitle icon="fa fa-cogs" main="Administração do Sistema"
+            sub="Cadastros & Questionários" />
+        <div class="admin-pages-tabs">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title="Categorias">
+                        <CategoryAdmin />
+                    </b-tab>
+                    <b-tab title="Usuários">
+                        <UserAdmin />
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
+
+export default {
+    name: 'AdminPages',
+    components: { PageTitle, CategoryAdmin, UserAdmin }
+}
+</script>
+
+<style>
+
+</style>
